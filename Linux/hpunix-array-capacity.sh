0a35426bfe5a85db600e14ed2eb4770f2f46c32e
kvrmuruganandam <48209321+kvrmuruganandam@users.noreply.github.com>
2020-01-17 00:29:17 +0530

    updated hpunix

--- Linux/hpunix-array-capacity.sh
+++ Linux/hpunix-array-capacity.sh
@@ -7,10 +7,7 @@
 # Author                :       "Muruganandam"
 # Created Date          :       "2.10.2019"
 # Latest Update         :       "9.10.2019"
-# Copyright             :       "Copyright 2019, The ZENfra Project"
-# License               :       "VTG"
 # Version               :       "1.0"
-# Email                 :       "migrationteam@virtualtechgurus.com"
 # Run Script            :       "./hpunix.sh"
 #
 # *********************************************
