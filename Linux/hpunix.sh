bc5aef37fbb6de4606ed6c615584ac2b7e2aa052
kvrmuruganandam <48209321+kvrmuruganandam@users.noreply.github.com>
2020-01-17 00:32:33 +0530

    updated hpunix

--- Linux/hpunix.sh
+++ Linux/hpunix.sh
@@ -4,13 +4,8 @@
 # ***    Hpunix Host Details                     ***               
 # **************************************************                
 # Script Name           :       "hpunix.sh"
-# Author                :       "Muruganandam"                            
-# Created Date          :       "2.10.2019"
-# Latest Update         :       "9.10.2019"
-# Copyright             :       "Copyright 2019, The ZENfra Project" 
-# License               :       "VTG"                                
-# Version               :       "1.0"                               
-# Email                 :       "migrationteam@virtualtechgurus.com"    
+# Author                :       "Muruganandam"                                  
+# Version               :       "1.0"                                  
 # Run Script            :       "./hpunix.sh"
 #
 # *********************************************
