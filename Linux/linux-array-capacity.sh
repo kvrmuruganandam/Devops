e933acd74e07789c78a1bdd4ad30a1a0a44887b7
kvrmuruganandam <48209321+kvrmuruganandam@users.noreply.github.com>
2020-01-17 00:33:12 +0530

    updated linux

--- Linux/linux-array-capacity.sh
+++ Linux/linux-array-capacity.sh
@@ -5,12 +5,7 @@
 # **************************************************
 # Script Name           :       "linux.sh"
 # Author                :       "Muruganandam"
-# Created Date          :       "2.10.2019"
-# Latest Update         :       "9.10.2019"
-# Copyright             :       "Copyright 2019, The ZENfra Project"
-# License               :       "VTG"
 # Version               :       "1.0"
-# Email                 :       "migrationteam@virtualtechgurus.com"
 # Run Script            :       "./linux.sh"
 #
 # *********************************************
